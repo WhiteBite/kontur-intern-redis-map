2c7b3dcbea349a175f1db2e34ccb1705bae714a0
WhiteBite <ad.lord9000@yandex.ru>
2020-07-25 02:25:32 +0300

    Update RedisMap.java

--- src/main/java/ru/gnkoshelev/kontur/intern/redis/map/RedisMap.java
+++ src/main/java/ru/gnkoshelev/kontur/intern/redis/map/RedisMap.java
@@ -1,70 +1,106 @@
-package ru.gnkoshelev.kontur.intern.redis.map;
+package ru.whitebite.redis.map;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
+import lombok.Getter;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
-/**
- * @author Gregory Koshelev
- */
-public class RedisMap implements Map<String,String> {
+import java.util.*;
+
+public class RedisMap implements Map<String, String> {
+ HashMap
+    private final JedisPool jedisPool = new JedisPool(new GenericObjectPoolConfig(), "localhost", 6379);
+    private Jedis jedisClient = jedisPool.getResource();
+
+    {
+        Runtime.getRuntime().addShutdownHook(new Thread(this::clear));
+    }
+
+    public RedisMap(String nameSpace) {
+        NameSpace = nameSpace;
+    }
+
+    public RedisMap() {
+        NameSpace = "RedisMap_" + UUID.randomUUID().toString();
+    }
+
+    @Getter
+    String NameSpace;
+
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return jedisClient.hlen(NameSpace).intValue();
     }
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException();
+        return size() == 0;
     }
+
 
     @Override
     public boolean containsKey(Object key) {
-        throw new UnsupportedOperationException();
+        return jedisClient.hexists(NameSpace, Objects.requireNonNull(key).toString());
     }
+
 
     @Override
     public boolean containsValue(Object value) {
-        throw new UnsupportedOperationException();
+        Collection<String> q = values();
+        for (String val : q) {
+            if (value.toString().equals(val))
+                return true;
+        }
+        return false;
     }
 
     @Override
     public String get(Object key) {
-        throw new UnsupportedOperationException();
+        return key != null ? jedisClient.hget(NameSpace, key.toString()) : null;
     }
 
     @Override
     public String put(String key, String value) {
-        throw new UnsupportedOperationException();
+        Objects.requireNonNull(key);
+        Objects.requireNonNull(value);
+        String current = get(key);
+        jedisClient.hset(NameSpace, key, value);
+        return current;
     }
+
 
     @Override
     public String remove(Object key) {
-        throw new UnsupportedOperationException();
+        String current = get(key);
+        if (key != null) {
+            jedisClient.hdel(NameSpace, key.toString());
+        }
+        return current;
     }
 
     @Override
     public void putAll(Map<? extends String, ? extends String> m) {
-        throw new UnsupportedOperationException();
+        jedisClient.hmset(NameSpace, (Map<String, String>) m);
     }
 
     @Override
     public void clear() {
-        throw new UnsupportedOperationException();
+        jedisClient.del(NameSpace);
     }
+
 
     @Override
     public Set<String> keySet() {
-        throw new UnsupportedOperationException();
+        return jedisClient.hkeys(NameSpace);
     }
 
     @Override
     public Collection<String> values() {
-        throw new UnsupportedOperationException();
+        return jedisClient.hvals(NameSpace);
     }
 
     @Override
     public Set<Entry<String, String>> entrySet() {
-        throw new UnsupportedOperationException();
+        return jedisClient.hgetAll(NameSpace).entrySet();
     }
 }
